ae4e88fa7123d32f2bf2c79c309d559a638a2d19
Nate Costello <natecostello@gmail.com>
2023-12-23 17:47:14 +0000

    created header

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,12 +1,19 @@
 import React from "react";
 import styled from "styled-components";
+import { Nav } from "./Nav"
 
 const ContainerWrapper = styled.div`
-  height: 100%;
-  display: block;
-  margin: auto;
-  max-width: 640px;
+height: 100%;
+display: block;
+margin: auto;
+max-width: 640px;
 `;
 export const Container = ({ children }) => {
-  return <ContainerWrapper>{children}</ContainerWrapper>;
-};
+    return (
+        <ContainerWrapper>
+        {"  "}
+        <Nav />
+        {children}
+        </ContainerWrapper>
+        );
+    };
