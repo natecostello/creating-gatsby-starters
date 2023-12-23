86ffcd2bb076ecc7844a19723552e144f4b6beff
Nate Costello <natecostello@gmail.com>
2023-12-23 16:22:49 +0000

    installed source-filesystem and google-fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,5 +8,34 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: ["Satisfy", "Montserrat", "JetBrains Mono"],
+        display: "swap"
+      }
+    },
+  ],
 }
