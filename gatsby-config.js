732bbed5bbd2fda98540885dc260158b8fb99b0e
Nate Costello <natecostello@gmail.com>
2023-12-24 06:35:49 +0000

    created seo component

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,27 @@
 /**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
- */
+* Configure your Gatsby site with this file.
+*
+* See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+*/
 
 const { resolve } = require('path');
 
 /**
- * @type {import('gatsby').GatsbyConfig}
- */
+* @type {import('gatsby').GatsbyConfig}
+*/
 module.exports = {
+  siteMetadata: {
+    title: `Gatsby Starter Blog`,
+    author: {
+      name: `Ashutosh Kumar Singh`,
+    },
+    description: `Gatsby Starter Blog built with MDX and Styled Components.`,
+    image: `/cover.png`,
+    siteUrl: `https://lelouchb-gatsby-starter-blog-demo.netlify.app/`,
+    social: {
+      twitter: `@noharashutosh`,
+    },
+  },
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
@@ -48,11 +60,11 @@ module.exports = {
         extensions: [`.md`, `.mdx`],
         gatsbyRemarkPlugins: [
           {
-          resolve: `gatsby-remark-images`,
-          options: {
-            maxWidth: 590,
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
           },
-        },
         ],
       }
     },
